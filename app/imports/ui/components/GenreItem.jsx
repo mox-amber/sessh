eb8a595a88080e586f885827a59f5a94d3c4f334
Kuliakaiwikuamoo Akioka <66751553+Kakioka@users.noreply.github.com>
2022-04-12 11:09:35 -1000

    GenreItem created

---
+++ app/imports/ui/components/GenreItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class GenreItem extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header as="h2">{this.props.genre.name}</Header>
+      </Container>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+GenreItem.propTypes = {
+  genre: PropTypes.shape({
+    name: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(GenreItem);
