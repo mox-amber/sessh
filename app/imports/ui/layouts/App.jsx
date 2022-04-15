53639745effc021709f59c3471310d5b3b2f77cd
ebirtodaso <edwardkb@hawaii.edu>
2022-04-14 20:03:19 -1000

    Added basic admin features and edit profile.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,8 +7,6 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import AddStuff from '../pages/AddStuff';
 import NotFound from '../pages/NotFound';
 import EditProfile from '../pages/EditProfile';
 import Signin from '../pages/Signin';
@@ -30,9 +28,9 @@ class App extends React.Component {
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
             <ProtectedRoute path="/edit-profile/" component={EditProfile}/>
-            <ProtectedRoute path="/list" component={ListStuff}/>
+            {/* <ProtectedRoute path="/list" component={ListStuff}/> */}
             <ProtectedRoute path="/search" component={SearchMusician}/>
-            <ProtectedRoute path="/add" component={AddStuff}/>
+            {/* <ProtectedRoute path="/add" component={AddStuff}/> */}
             <AdminProtectedRoute path="/admin" component={ListMusiciansAdmin}/>
             <Route component={NotFound}/>
           </Switch>
