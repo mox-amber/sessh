6f04b547a1aec260a57ec5ccdca5aced31e144ea
Kuliakaiwikuamoo Akioka <66751553+Kakioka@users.noreply.github.com>
2022-04-21 10:46:29 -1000

    AddMusician page created and works. Can add stuff into MusiciansCollection

---
+++ app/imports/ui/pages/AddMusician.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, NumField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Musicians } from '../../api/musician/Musician';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  name: String,
+  age: Number,
+  image: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
+class AddMusician extends React.Component {
+
+  // On submit, insert the data.
+  submit(data, formRef) {
+    const { name, age, image } = data;
+    const owner = Meteor.user().username;
+    Musicians.collection.insert({ name, age, image, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Musician added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  render() {
+    let fRef = null;
+    return (
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center">Add Musician</Header>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+            <Segment>
+              <TextField name='name'/>
+              <NumField name='age' decimal={false}/>
+              <TextField name='image'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default AddMusician;
